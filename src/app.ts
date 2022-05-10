57536ff5cf726900a98f46dc2008b25e7f3cbeec
dcollis92 <davidphilipcollis@gmail.com>
2022-05-10 10:10:03 -0500

    interacting with dom elements

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 // ---- DOM ELEMENT SELECTION & OOP RENDERING -----  \\
 // -------- INTERACTING WITH DOM ELEMENTS ---------  \\
 
-
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -18,7 +20,29 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
@@ -29,5 +53,13 @@ class ProjectInput {
 const prjInput = new ProjectInput();
 
 // -------- INTERACTING WITH DOM ELEMENTS ---------  \\
+// line 23 added to identify element id
+// line 8-10 added new elements
+// line 25-33 attach id to elements
+// line 44 configure method
+
 // ---- CREATING & USING AN AUTOBIND DECORATOR ----  \\
+
+
 // ------------- FETCHING USER INPUT --------------  \\
+// -- CREATING A RE-USABLE VALIDATION FUNCTIONALITY --  \\
